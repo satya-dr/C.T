d3e0d7f474ad39b2872e60fd68d56b705032834a
SATYAJIT ROY <satyajitroy19599@gmail.com>
2025-03-28 01:06:56 +0530

    Updates

---
+++ precache-manifest.b651f6e3bfc8739aa0e3c9ddf21bb88b.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e21709ae54ff19d87de2bad50894580f",
+    "url": "/cgectimes/index.html"
+  },
+  {
+    "revision": "bcbbd5cfe08c269d082b",
+    "url": "/cgectimes/static/css/main.a6e4d399.chunk.css"
+  },
+  {
+    "revision": "00ea069a9fae41e41d73",
+    "url": "/cgectimes/static/js/2.e0e1ae4f.chunk.js"
+  },
+  {
+    "revision": "ea862b4d7291d9d99556",
+    "url": "/cgectimes/static/js/3.bd0f349a.chunk.js"
+  },
+  {
+    "revision": "bcbbd5cfe08c269d082b",
+    "url": "/cgectimes/static/js/main.9b00ed63.chunk.js"
+  },
+  {
+    "revision": "6372776e27b13ef6bcb0",
+    "url": "/cgectimes/static/js/runtime~main.66744d0d.js"
+  },
+  {
+    "revision": "a40fdbf0f2719211bf2fe1c38df80af5",
+    "url": "/cgectimes/static/media/NO1.a40fdbf0.jpg"
+  },
+  {
+    "revision": "0bc1e549f0daba076bdfa2afb1d9be5c",
+    "url": "/cgectimes/static/media/NO2.0bc1e549.jpg"
+  },
+  {
+    "revision": "c1fc0fe560a39024def57d992db73fce",
+    "url": "/cgectimes/static/media/NO3.c1fc0fe5.jpg"
+  },
+  {
+    "revision": "d0e2ba2ab796484e8fc03f9db8771d6d",
+    "url": "/cgectimes/static/media/art1.d0e2ba2a.jpg"
+  },
+  {
+    "revision": "588aacb2b466a1d2f58a961b63ac37c6",
+    "url": "/cgectimes/static/media/art2.588aacb2.jpg"
+  },
+  {
+    "revision": "c05b5d1bf9488b7323094a2e2dd1703b",
+    "url": "/cgectimes/static/media/art3.c05b5d1b.jpg"
+  },
+  {
+    "revision": "005ad1727ac5e63cee93253502a89718",
+    "url": "/cgectimes/static/media/art4.005ad172.jpg"
+  },
+  {
+    "revision": "b395d33e461c917d25be5e2005a4fad7",
+    "url": "/cgectimes/static/media/art5.b395d33e.jpg"
+  },
+  {
+    "revision": "65b2b8cb1340fc1003f3e943cc93438b",
+    "url": "/cgectimes/static/media/art6.65b2b8cb.jpg"
+  },
+  {
+    "revision": "1f7369cea36ebea60391c8a048b0f8b1",
+    "url": "/cgectimes/static/media/cgec_acdemic.1f7369ce.jpeg"
+  },
+  {
+    "revision": "e394d69733c9038f9d4883074d1503f3",
+    "url": "/cgectimes/static/media/cgectimes.e394d697.png"
+  },
+  {
+    "revision": "a3fdaa3924672ac3678c39ee6f203bc5",
+    "url": "/cgectimes/static/media/clgLogo.a3fdaa39.png"
+  },
+  {
+    "revision": "607b9f69862d76af04b474113c0c7ff5",
+    "url": "/cgectimes/static/media/india.607b9f69.png"
+  }
+]);
