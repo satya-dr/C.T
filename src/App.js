3474b1f759489af7c45de333b161d83b8705d56d
SATYAJIT ROY <satyajitroy19599@gmail.com>
2025-03-28 00:49:00 +0530

    Add all

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
 import './App.css';
 import React from 'react';
 import NavBar from './components/NavBar';
-import HeaderSection from './components/HeaderSection';
+import About from './components/About';
+import NewsPage from './components/NewsPage'; 
+import ClubSection from './components/ClubSection'; 
+import EventSection from './components/EventSection'; 
+import TeamPage from './components/TeamPage'; 
+
 
 
 function App() {
   return (
     <div className="App">
       <NavBar/>
-      <HeaderSection/>
+      <About/>
+      <NewsPage/>
+      <ClubSection/>
+      <EventSection/>
+      <TeamPage/>
+       
+      
     </div>
   );
 }
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -6,8 +6,21 @@ import logo from './photos/cgectimes.png';
 import mainLogo from './photos/ct.jpg';
 
 const NavBar = () => {
+    const getCurrentDate = () => {
+        const options = { 
+          weekday: 'long', 
+          year: 'numeric', 
+          month: 'short', 
+          day: 'numeric' 
+        };
+        const today = new Date();
+        return today.toLocaleDateString('en-US', options);
+      };
+    
+      // Function to get current time (AM/PM format)
+      
     return (
-        <>
+        <div className='main_nav'>
             <div className="fixed-top bg-white">
                 <nav className="navbar navbar-expand-lg navbar-light">
                     <div className="head container-fluid">
@@ -22,7 +35,7 @@ const NavBar = () => {
                         </div>
 
                         <div className="d-flex align-items-center">
-                            <div className="nav-item email-btn"><a href="#">Email<i className="fa-solid fa-envelope mx-1"></i></a> </div>
+                            <div className="nav-item email-btn"><a href="/Email">Email<i className="fa-solid fa-envelope mx-1"></i></a> </div>
                             <a href="https://www.linkedin.com/company/the-cgec-times/"><i className="fa-brands fa-linkedin-in mx-2 linkedIn social-icon"></i></a>
                             <a href="https://www.instagram.com/the_cgec_times/"><i className="fa-brands fa-instagram mx-2 instaGram social-icon"></i></a>
                             <a href="https://www.facebook.com/people/The-CGEC-TIMES/100088137340330/"><i className="fa-brands fa-facebook-f mx-2 faceBook social-icon"></i></a>
@@ -36,9 +49,10 @@ const NavBar = () => {
                 <div className="row">
                     <div className="col-md-12">
                         <div className="d-flex flex-column flex-md-row justify-content-between align-items-center p-3">
-                            <div className='dateDetails d-flex justify-content-center align-items-center mb-3 mb-md-0'>
-                                <u>Wednesday, 29 jan. 2025 || e-Paper || TODAY’S PAPER <br /> ENGLISH|বাংলা|हिंदी </u>
-                            </div>
+                        <div className='dateDetails d-flex justify-content-center align-items-center mb-3 mb-md-0'>
+                          <u> {getCurrentDate()} || e-Paper || TODAY'S PAPER <br />  ENGLISH | বাংলা | हिंदी 
+                          </u>
+                        </div>
                             <div className='clg_logos d-flex mb-3 mb-md-0'>
                                 <img className='main_logo' src={mainLogo} alt="" />
                                 <img className='cgec' src={logo} alt="" />
@@ -77,10 +91,8 @@ const NavBar = () => {
             <div className="margin"></div>
 
             
-            <div style={{ paddingTop: '300px' }}>
-                
-            </div>
-        </>
+            
+        </div>
     );
 };
 
